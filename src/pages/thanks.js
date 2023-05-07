ff1960936fc6067426b84293ac7a12738fbaa746
Jason Prillo <jasonprillo@gmail.com>
2023-05-07 16:05:19 -0400

    Create thanks.js

---
+++ src/pages/thanks.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+
+
+const Thanks = () => {
+
+
+  return (
+    <Layout>
+<section className="light-back1">
+    <h1>Thanks You </h1>
+    <p>We will get back to you as soon as possible.</p>
+</section>
+    </Layout>
+  );
+};
+
+export default Thanks;
